a1e07e7fc135d9da263726ecee1d63c063e585cd
Andrew Jones <ascjones@gmail.com>
2012-02-08 09:07:55 +0000

    emit tuples from Protocol

--- StormNet.Tests/ProtocolTests.cs
+++ StormNet.Tests/ProtocolTests.cs
@@ -1,9 +1,34 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using ServiceStack.Text;
 
 namespace StormNet.Tests
 {
     [TestFixture]
     public class ProtocolTests
     {
+        [Test]
+        public void EmitTuple()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw); // redirect stdout to my writer
+
+                using (var sr = new StringReader("end" + Environment.NewLine))
+                {
+                    Console.SetIn(sr);
+
+                    Protocol.Emit(new object[] { "x" });
+
+                    var emittedMessage = sw.ToString();
+                    var json = JsonObject.Parse(emittedMessage);
+
+                    Assert.That(json["command"], Is.EqualTo("emit"), "The command name should be 'emit'");
+                    Assert.That(json["anchors"], Is.EqualTo("[]"), "No anchors were specified, the list should be empty");
+                    Assert.That(json.Get<object[]>("tuple"), Is.EquivalentTo(new object[] { "x" }));
+                }
+            }
+        }
     }
 }
--- StormNet/Protocol.cs
+++ StormNet/Protocol.cs
@@ -1,11 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using ServiceStack.Text;
 
 namespace StormNet
 {
     public class Protocol
     {
+        private static string ReadStringMessage()
+        {
+            string msg = String.Empty;
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "end")
+                    break;
+
+                msg += line + "\n";
+            }
+            return msg.TrimEnd('\n');
+        }
+
+        private static JsonObject ReadMessage()
+        {
+            string message = ReadStringMessage();
+            return JsonObject.Parse(message);
+        }
+
+        private static void SendToParent(string s)
+        {
+            Console.Out.WriteLine(s);
+            Console.Out.WriteLine("end");
+            Console.Out.Flush();
+        }
+
+        private static void SendMessageToParent(object json)
+        {
+            SendToParent(json.ToJson());
+        }
+
+        private static void EmitTuple(object[] tuple, int? streamId = null, IEnumerable<StormTuple> anchors = null, long? direct = null)
+        {
+            long[] anchorIds = anchors != null ? anchors.Select(a => a.Id).ToArray() : new long[] { };
+
+            var message = new Dictionary<string, object>
+            {
+                {"command", "emit"},
+                {"anchors", anchorIds},
+                {"tuple", tuple}
+            };
+
+            if (streamId != null) message.Add("stream", streamId);
+            if (direct != null) message.Add("task", direct);
+
+            SendMessageToParent(message);
+        }
+
+        public static JsonObject Emit(object[] tuple, int? streamId = null, IEnumerable<StormTuple> anchors = null)
+        {
+            EmitTuple(tuple, streamId, anchors);
+            return ReadMessage();
+        }
     }
 }
